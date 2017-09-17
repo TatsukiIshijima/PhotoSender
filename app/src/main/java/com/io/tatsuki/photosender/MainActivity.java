ad1093004867bd9c6b89b5621238976fd12a1450
TatsukiIshijima <tatsuki445@gmail.com>
2017-09-17 18:44:31 +0900

    画像保存機能実装

--- app/src/main/java/com/io/tatsuki/photosender/MainActivity.java
+++ app/src/main/java/com/io/tatsuki/photosender/MainActivity.java
@@ -1,8 +1,17 @@
 package com.io.tatsuki.photosender;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +20,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -24,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int RESULT_CAMERA = 1001;
-    private static final String BASE_URL = "";
+    private final static int REQUEST_PERMISSION = 1002;
 
     private ImageView mTakePhotoImageView;
     private Button mShootButton;
@@ -32,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private String mImagePath;
     private File mImageFile;
+    private Uri mImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +57,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mShootButton.setOnClickListener(this);
         mSendButton.setOnClickListener(this);
 
-        // カメラ起動
-        startActivityForResult(cameraIntent(), RESULT_CAMERA);
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermission();
+        }
+        else {
+            // カメラ起動
+            startActivityForResult(cameraIntent(), RESULT_CAMERA);
+        }
     }
 
     /**
@@ -59,8 +76,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == RESULT_CAMERA) {
             // 画像を取得し、セットする
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-            mTakePhotoImageView.setImageBitmap(bitmap);
+            if (mImageUri != null) {
+                mTakePhotoImageView.setImageURI(mImageUri);
+                registerDatabase(mImagePath);
+            }
+        }
+    }
+
+    /**
+     * パーミッションの取得結果
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startActivityForResult(cameraIntent(), RESULT_CAMERA);
+            } else {
+                finish();
+            }
+        }
+    }
+
+    /**
+     * パーミッションの確認
+     */
+    private void checkPermission(){
+        // 既に許可している
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+            // カメラ起動
+            startActivityForResult(cameraIntent(), RESULT_CAMERA);
+        }
+        // 拒否していた場合
+        else{
+            requestPermission();
+        }
+    }
+
+    /**
+     * パーミッションの要求
+     */
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,}, REQUEST_PERMISSION);
         }
     }
 
@@ -69,8 +132,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * @return intent
      */
     private Intent cameraIntent() {
+        createImagePath();
+        mImageFile = new File(mImagePath);
+        mImageUri = FileProvider.getUriForFile(MainActivity.this, getApplicationContext().getPackageName() + ".provider", mImageFile);
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
         return intent;
+    }
+
+    /**
+     * 画像ファイルのパス作成
+     * @return
+     */
+    private void createImagePath() {
+        // 保存先のフォルダーを作成
+        File cameraFolder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "IMG");
+        cameraFolder.mkdirs();
+
+        // 保存ファイル名
+        String fileName = new SimpleDateFormat("ddHHmmss").format(new Date());
+        mImagePath = cameraFolder.getPath() +"/" + fileName + ".jpg";
+    }
+
+    /**
+     * DBへパスを登録する
+     * @param path
+     */
+    private void registerDatabase(String path) {
+        ContentValues contentValues = new ContentValues();
+        ContentResolver contentResolver = MainActivity.this.getContentResolver();
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        contentValues.put("_data", path);
+        contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,contentValues);
     }
 
     /**
@@ -86,20 +179,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             // 送信ボタン
             case R.id.send_button:
-                UploadService service = ServiceGenerator.createService(UploadService.class);
-                RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), mImageFile);
-                Call<String> call = service.upload(requestBody);
-                call.enqueue(new Callback<String>() {
-                    @Override
-                    public void onResponse(Call<String> call, Response<String> response) {
-                        Log.d(TAG, "onSuccess : " + response);
-                    }
+                if (mImageFile != null) {
+                    UploadService service = ServiceGenerator.createService(UploadService.class);
+                    RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), mImageFile);
+                    Call<String> call = service.upload(requestBody);
+                    call.enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            Log.d(TAG, "onSuccess : " + response);
+                        }
 
-                    @Override
-                    public void onFailure(Call<String> call, Throwable t) {
-                        Log.d(TAG, "onFailure");
-                    }
-                });
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+                            Log.d(TAG, "onFailure");
+                        }
+                    });
+                } else {
+                    Log.d(TAG, "Image File is NULL");
+                }
+
                 break;
         }
     }
